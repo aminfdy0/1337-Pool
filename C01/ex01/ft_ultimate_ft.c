7ae17e87a6867cca8caa0338e1598c323a040be4
aminfdy0 <aminefdy@gmail.com>
2026-02-19 15:17:30 +0000

    ex01 - Pointer To Pointer

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdycoding <aminefdy@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/19 14:58:08 by fdycoding         #+#    #+#             */
+/*   Updated: 2026/02/19 14:59:33 by fdycoding        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+int main(void)
+{
+    int fdy = 97;
+
+    int *one = &fdy;
+    int **two = &one;
+    int ***three = &two;
+    int ****four = &three;
+    int *****five = &four;
+    int ******six = &five;
+    int *******se7en = &six;
+    int ********eight = &se7en;
+    int *********nine = &eight;
+    ft_ultimate_ft(nine);
+    // Size Of Pointer Is 8 bytes on 64-bit system
+    //                    4 bytes on 32-bit system
+    printf("%i\n", fdy);
+}
+*/

cdb4115aea3bd95b48e45e5bc46f88ec8e460400
aminfdy0 <aminefdy@gmail.com>
2026-02-25 14:08:15 +0000

    ex06 - Count Len Of String

---
+++ C01/ex06/ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdycoding <aminefdy@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/25 13:59:17 by fdycoding         #+#    #+#             */
+/*   Updated: 2026/02/25 14:07:05 by fdycoding        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/*
+int main(void)
+{
+    char     serie[] = "Better Call Saul";
+    printf("%i\n", ft_strlen(serie));
+}
+*/

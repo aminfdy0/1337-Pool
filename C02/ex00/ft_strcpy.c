25cd342aa0e552a57bc18aeace94de0183af950b
aminfdy0 <aminefdy@gmail.com>
2026-02-26 14:30:55 +0000

    ex00 - Strcpy

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdycoding <aminefdy@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/26 14:05:51 by fdycoding         #+#    #+#             */
+/*   Updated: 2026/02/26 14:18:23 by fdycoding        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int main(void)
+{
+    char org[] = "FeDaYee";
+    char cpy[sizeof(org)];
+    printf("%s", ft_strcpy(cpy, org));    
+}
+*/

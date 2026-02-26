6d0cc2b108eb20cc8589b75e9bc9401f26a77211
aminfdy0 <aminefdy@gmail.com>
2026-02-26 14:43:20 +0000

    ex01 - Strncpy

---
+++ C02/ex01/strncpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strncpy.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdycoding <aminefdy@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/26 14:33:31 by fdycoding         #+#    #+#             */
+/*   Updated: 2026/02/26 14:41:35 by fdycoding        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int main(void)
+{
+    char org[] = "FeDaYee";
+    char cpy[sizeof(org)];
+    int num = 15;
+    printf("%s", ft_strncpy(cpy, org, num));    
+}
+*/

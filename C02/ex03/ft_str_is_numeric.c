35342b59fad350b9d588365e4758921fca701ed2
aminfdy0 <aminefdy@gmail.com>
2026-03-02 16:20:33 +0000

    ex03 - isdigit

---
+++ C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdycoding <aminefdy@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 16:14:12 by fdycoding         #+#    #+#             */
+/*   Updated: 2026/03/02 16:17:12 by fdycoding        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/*
+int main()
+{
+    char name[] = "1312";
+    printf("%i", ft_str_is_numeric(name));
+}
+*/

b6fe05b826813df0175ac66f474f9bd2b408ac0e
aminfdy0 <aminefdy@gmail.com>
2026-03-02 16:47:52 +0000

    ex07 - converts every letter to uppercase

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdycoding <aminefdy@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 16:42:00 by fdycoding         #+#    #+#             */
+/*   Updated: 2026/03/02 16:44:24 by fdycoding        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
+/*
+int main(void)
+{
+	char	name[] = "fedayee0";
+	printf("%s", ft_strupcase(name));
+}
+*/

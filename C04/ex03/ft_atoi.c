0413ed4777952496f994036611356bf4c53f083f
aminfdy0 <aminefdy@gmail.com>
2026-03-09 17:53:55 +0000

    ex03 - ft_atoi

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdycoding <aminefdy@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 17:47:26 by fdycoding         #+#    #+#             */
+/*   Updated: 2026/03/09 17:50:39 by fdycoding        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	number;
+
+	sign = 0;
+	number = 0;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign++;
+		str++;
+	}
+	while (*str >= 48 && *str <= 57)
+	{
+		number *= 10;
+		number += *str - 48;
+		str++;
+	}
+	if (!(sign % 2))
+		return (number);
+	return (-number);
+}
+/*
+int main(void)
+{
+    printf("%i\n", ft_atoi("  --+-3lch Ma9ritix"));
+}
+*/
